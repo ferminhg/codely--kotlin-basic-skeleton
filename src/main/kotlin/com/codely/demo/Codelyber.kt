bfde8375a7d9435163d03560540b7bc936713322
Fermin <josefermin.hernandez@adevinta.com>
2023-04-07 08:09:22 +0200

    step2: playing with nulls

--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -1,5 +1,7 @@
 package com.codely.demo
 
+import java.time.LocalDate
+
 const val WELCOME_MESSAGE = "Welcome to kotlin skeleton!!!"
 
 class Codelyber {
@@ -7,5 +9,13 @@ class Codelyber {
 }
 
 fun main() {
-    println(Codelyber().greet())
+    println("Please enter a date in the format <yyyy-mm-dd>:")
+    val enteredDate: String? = null
+//    val date = LocalDate.parse(enteredDate ?: "1982-11-21")
+//    val date = LocalDate.parse(enteredDate!!)
+
+    enteredDate?.let {
+        val date = LocalDate.parse(it)
+        println("The date you entered is: $date and the day of the week is: ${date.dayOfWeek}")
+    }
 }
